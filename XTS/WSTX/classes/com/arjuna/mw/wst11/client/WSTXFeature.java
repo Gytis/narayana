2625cef92ab88f79786d87b7fb62b3fd4766f8d5
Gytis Trikleris <gytist@gmail.com>
2013-02-26 14:57:59 +0000

    Raw strings moved to static fields

--- XTS/WSTX/classes/com/arjuna/mw/wst11/client/WSTXFeature.java
+++ XTS/WSTX/classes/com/arjuna/mw/wst11/client/WSTXFeature.java
@@ -1,23 +1,23 @@
 /*
-* JBoss, Home of Professional Open Source
-* Copyright 2013, Red Hat Middleware LLC, and individual contributors
-* as indicated by the @author tags.
-* See the copyright.txt in the distribution for a full listing
-* of individual contributors.
-* This copyrighted material is made available to anyone wishing to use,
-* modify, copy, or redistribute it subject to the terms and conditions
-* of the GNU Lesser General Public License, v. 2.1.
-* This program is distributed in the hope that it will be useful, but WITHOUT A
-* WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
-* PARTICULAR PURPOSE. See the GNU Lesser General Public License for more details.
-* You should have received a copy of the GNU Lesser General Public License,
-* v.2.1 along with this distribution; if not, write to the Free Software
-* Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
-* MA 02110-1301, USA.
-*
-* (C) 2005-2006,
-* @author JBoss Inc.
-*/
+ * JBoss, Home of Professional Open Source
+ * Copyright 2013, Red Hat Middleware LLC, and individual contributors
+ * as indicated by the @author tags.
+ * See the copyright.txt in the distribution for a full listing
+ * of individual contributors.
+ * This copyrighted material is made available to anyone wishing to use,
+ * modify, copy, or redistribute it subject to the terms and conditions
+ * of the GNU Lesser General Public License, v. 2.1.
+ * This program is distributed in the hope that it will be useful, but WITHOUT A
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+ * PARTICULAR PURPOSE. See the GNU Lesser General Public License for more details.
+ * You should have received a copy of the GNU Lesser General Public License,
+ * v.2.1 along with this distribution; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301, USA.
+ *
+ * (C) 2005-2006,
+ * @author JBoss Inc.
+ */
 
 package com.arjuna.mw.wst11.client;
 
@@ -26,10 +26,16 @@ import javax.xml.ws.BindingProvider;
 import org.jboss.ws.api.configuration.AbstractClientFeature;
 
 /**
-* @author <a href="mailto:paul.robinson@redhat.com">Paul Robinson</a>
-* @author <a href="mailto:gytis@redhat.com">Gytis Trikleris</a>
-*/
+ * @author <a href="mailto:paul.robinson@redhat.com">Paul Robinson</a>
+ * @author <a href="mailto:gytis@redhat.com">Gytis Trikleris</a>
+ */
 public final class WSTXFeature extends AbstractClientFeature {
+
+    public static final String REQUEST_CONTEXT_KEY = "XTS";
+
+    public static final String ENABLED_VALUE = "true";
+
+    public static final String DISABLED_VALUE = "false";
 
     protected boolean enabled = true;
 
@@ -45,9 +51,13 @@ public final class WSTXFeature extends AbstractClientFeature {
     @Override
     protected void initializeBindingProvider(BindingProvider bp) {
         if (enabled) {
-            bp.getRequestContext().put("XTS", "true");
+            bp.getRequestContext().put(REQUEST_CONTEXT_KEY, ENABLED_VALUE);
         } else {
-            bp.getRequestContext().put("XTS", "false");
+            if (!bp.getRequestContext().containsKey(REQUEST_CONTEXT_KEY)) {
+                // Disable handler only if another feature does not require it.
+                bp.getRequestContext().put(REQUEST_CONTEXT_KEY, DISABLED_VALUE);
+            }
         }
     }
+
 }
